b87c7197c7dd69f92e310b33dbafdbc835c520f0
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-12-21 08:40:27 +0200

    slices added

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { StyledLi } from './ContactItem.styled';
 import { StyledButton, StyledSpanName, StyledSpanNumber } from '../ContactList.styled';
 import { useDispatch } from 'react-redux';
--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <PersistGate loading={null} >
       <App />
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
